8aca1f492bb637729c800fd6790c75d432b9db72
attocubedev <attocube.dev@gmail.com>
2016-07-15 06:04:13 +0000

    migration FIles

--- homepage/models.py
+++ homepage/models.py
@@ -8,18 +8,15 @@ from django.db.models.signals import post_save
 
 class SliderImages(models.Model):
 	imgId = models.AutoField(primary_key=True)
-	#imgPath = models.ImageField(upload_to='homepage/') #upload_to seems not working
 	imgPath = models.CharField(max_length=256)
 	isAppear = models.BooleanField(default=True)
 	imgName = models.CharField(max_length=100)
 	role = models.PositiveSmallIntegerField(default=0)
- 	#maybe role will be change to user.group
 
 	def __str__(self):
 		return self.imgName
 	#registDate = models.DateField.auto_now
 	#expireDate = models.DateField
-	#imgPath? , model.ImageField(upload_to='.....') #file will be saved to MEDIA_ROOT/.... 
 
 class userProfile(models.Model):
 	def create_user_profile(sender, instance, created, **kwargs):
@@ -32,12 +29,12 @@ class userProfile(models.Model):
 
 
 	user = models.OneToOneField(settings.AUTH_USER_MODEL ,on_delete=models.CASCADE, parent_link=True, primary_key=True)
-	#maybe user.username is id and, first_name/ last_name is real name
+	#user.username is id and, first_name/ last_name is real name
 	address_line1 = models.CharField(max_length=100,null=True)
 	address_line2 = models.CharField(max_length=100,null=True)
 	mobilePhoneNumber = models.CharField(max_length=50,null=True)
 	phoneNumber = models.CharField(max_length=50,null=True)
-	role = models.IntegerField(null=True)
+	role = models.PositiveSmallIntegerField(default=0, null=False)
 	post_save.connect(create_user_profile, sender=User)
 
 	def __str__(self):
