06b5e63d00c6c581975a8e91a5cabb78b67b490d
Zudog <Zudog@Zudogui-MacBook-Pro.local>
2016-07-25 17:45:52 +0900

    TT

---
+++ application/qa/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+import django.contrib.auth.views as auth_views
+from . import views
+
+# base
+urlpatterns = [
+	url(r'^$',views.qa, name='qa'),
+]
+
