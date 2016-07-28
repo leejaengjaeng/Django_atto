6f76bb2553f8b0ef089f776f00a813876e6ec15a
leejaengjaeng <leejaengjaeng@naver.com>
2016-07-28 14:40:57 +0900

    news 만드는중

--- application/news/admin.py
+++ application/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Posts, Comments
 # Register your models here.
+
+
+# Re register UserAdmin
+admin.site.register(Posts)
+admin.site.register(Comments)
+
--- application/news/views.py
+++ application/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+import application.common.customUserHandler as cuh
 
 # Create your views here.
 def news(request):
-    return render(request, "news/news.html")
+    posts = cuh.getPosts(request)
+    return render(request, "news/news.html",{'posts':posts})
