2b0eb6cd4fbfb05e05472383d95fcb91738ce2b6
?? ? <?? ?>
2016-07-28 11:30:16 +0900

    aa

--- application/download/admin.py
+++ application/download/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from application.download.models import singcontents
 # Register your models here.
+admin.site.register(singcontents)
--- application/download/models.py
+++ application/download/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class singcontents(models.Model):
+    video=models.FileField(upload_to='singcontents')
+    text=models.CharField(max_length=200)
+    source=models.URLField(max_length=300)
